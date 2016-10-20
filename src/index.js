4d3492f74eb3a94a25073240e75fa0a0c08664c4
Maxwell O'Roark <m.d.oroark@gmail.com>
2016-10-20 10:58:13 -0600

    changing POST url to /gbdx

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => { res.send('\n 👋 🌍 \n') })
 
-app.post('/commands/starbot', (req, res) => {
+app.post('/commands/gbdx', (req, res) => {
   let payload = req.body
 
   if (!payload || payload.token !== config('STARBOT_COMMAND_TOKEN')) {

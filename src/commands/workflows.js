128bb6773196d5271bb4452942bce40ffae97174
Maxwell O'Roark <m.d.oroark@gmail.com>
2016-10-20 12:39:51 -0600

    added access token, added api call to workflows, added parsing of response and logic

--- src/commands/workflows.js
+++ src/commands/workflows.js
@@ -3,6 +3,12 @@
 
 const _ = require('lodash')
 const config = require('../config')
+const request = require('request')
+const token= config('GBDX_ACCESS_TOKEN')
+
+console.log("GBDX TOKEN")
+console.log(token)
+
 
 const msgDefaults = {
   response_type: 'in_channel',
@@ -10,24 +16,48 @@ const msgDefaults = {
   icon_emoji: config('ICON_EMOJI')
 }
 
-let attachments = [
+let emptyAttachment = [
   {
     title: 'WORKFLOWS:',
     color: '#e67e22',
-    text: '["workflow1, workflow2, workflow3, workflow4"]',
+    text: 'oops, you have no workflows to show',
     mrkdwn_in: ['text']
   }
 ]
 
-const handler = (payload, res) => {
-  let msg = _.defaults({
-    channel: payload.channel_name,
-    attachments: attachments
-  }, msgDefaults)
+// each attachment will be one workflow plug the info into this template.  color can be variable
+// based on the status of the workflow and the text will be the actual text
+// title will be the ID of the workflow obviously.
 
-  res.set('content-type', 'application/json')
-  res.status(200).json(msg)
-  return
+const handler = (payload, res) => {
+
+    request
+        .get('https://geobigdata.io/workflows/v1/workflows', {
+            'auth': {'bearer': token}
+        })
+        .on('response', function(response) {
+            let workflows = response.body.Workflows
+            if (response.statusCode === 200 && workflows.length > 0) {
+                let attachments = workflows.slice(0, 5).map((workflow) => {
+                  return {
+                    title: `${workflow} `,
+                    title_link: `workflow link`,
+                    text: `some random text`,
+                    mrkdwn_in: ['text', 'pretext']
+                  }
+                })
+            } else {
+                let attachments = emptyAttachment
+            }
+            let msg = _.defaults({
+                channel: payload.channel_name,
+                attachments: attachments
+            }, msgDefaults)
+
+            res.set('content-type', 'application/json')
+            res.status(200).json(msg)
+            return
+        })
 }
 
 module.exports = { pattern: /workflows/ig, handler: handler }
--- src/config.js
+++ src/config.js
@@ -13,6 +13,7 @@ const config = {
   WEBHOOK_URL: process.env.WEBHOOK_URL,
   STARBOT_COMMAND_TOKEN: process.env.STARBOT_COMMAND_TOKEN,
   SLACK_TOKEN: process.env.SLACK_TOKEN,
+  GBDX_ACCESS_TOKEN: process.env.GBDX_ACCESS_TOKEN
   ICON_EMOJI: ':stars:'
 }
 

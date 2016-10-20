b3e02609a631a8884ec4b0754421a4333165b85e
Maxwell O'Roark <m.d.oroark@gmail.com>
2016-10-20 11:34:43 -0600

    bug fix

---
+++ src/commands/workflows.js
@@ -0,0 +1,33 @@
+
+'use strict'
+
+const _ = require('lodash')
+const config = require('../config')
+
+const msgDefaults = {
+  response_type: 'in_channel',
+  username: 'Starbot',
+  icon_emoji: config('ICON_EMOJI')
+}
+
+let attachments = [
+  {
+    title: 'WORKFLOWS:',
+    color: '#e67e22',
+    text: '["workflow1, workflow2, workflow3, workflow4"]',
+    mrkdwn_in: ['text']
+  }
+]
+
+const handler = (payload, res) => {
+  let msg = _.defaults({
+    channel: payload.channel_name,
+    attachments: attachments
+  }, msgDefaults)
+
+  res.set('content-type', 'application/json')
+  res.status(200).json(msg)
+  return
+}
+
+module.exports = { pattern: /workflows/ig, handler: handler }

442c4a324379cbb6bde28dd451f01aa66a2cc102
Maxwell O'Roark <m.d.oroark@gmail.com>
2016-10-20 13:05:55 -0600

    changed get request

--- src/commands/workflows.js
+++ src/commands/workflows.js
@@ -25,41 +25,38 @@ let emptyAttachment = [
   }
 ]
 
+let msg = _.defaults({
+    channel: payload.channel_name,
+    attachments: emptyAttachment
+}, msgDefaults)
+
 // each attachment will be one workflow plug the info into this template.  color can be variable
 // based on the status of the workflow and the text will be the actual text
 // title will be the ID of the workflow obviously.
 
 const handler = (payload, res) => {
 
-    request
-        .get('https://geobigdata.io/workflows/v1/workflows', {
-            'auth': {'bearer':token}
-        })
-        .on('response', function(response) {
-            console.log("===response===")
-            console.log(response.body)
-            let workflows = response.body.Workflows
-            if (response.statusCode === 200 && workflows.length > 0) {
-                let attachments = workflows.slice(0, 5).map((workflow) => {
-                  return {
-                    title: `${workflow} `,
-                    title_link: `workflow link`,
-                    text: `some random text`,
-                    mrkdwn_in: ['text', 'pretext']
-                  }
-                })
-            } else {
-                let attachments = emptyAttachment
-            }
-            let msg = _.defaults({
-                channel: payload.channel_name,
-                attachments: attachments
-            }, msgDefaults)
+    let options = {
+        url: 'https://geobigdata.io/workflows/v1/workflows',
+        headers: {
+            'Bearer' : token
+        }
+    }
 
-            res.set('content-type', 'application/json')
-            res.status(200).json(msg)
-            return
-        })
+    function callback(error, response, body) {
+        if (!error && response.statusCode === 200) {
+            var info = JSON.parse(body)
+            console.log("INFO")
+            console.log(info)
+        }
+    }
+
+    request(options, callback)
+
+    res.set('content-type', 'application/json')
+    res.status(200).json(msg)
+    return
+
 }
 
 module.exports = { pattern: /workflows/ig, handler: handler }

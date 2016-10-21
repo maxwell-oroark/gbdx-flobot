b4dfa5951f77fed955236c93a6bfbd28452fb9b1
Maxwell O'Roark <m.d.oroark@gmail.com>
2016-10-21 11:37:52 -0600

    added order cat id

---
+++ src/commands/order.js
@@ -0,0 +1,67 @@
+'use strict'
+
+const _ = require('lodash')
+const config = require('../config')
+const request = require('request')
+const token = config('GBDX_ACCESS_TOKEN')
+const moment = require('moment')
+
+
+
+const handler = (payload, res) => {
+
+    let catId = payload.text.match(/\d+/)
+
+    console.log("====CAT ID===")
+    console.log(catId)
+
+    const constructionAttachment = [
+        {
+            color: '#f1c40f',
+            text: 'this response is under construction but your cat id is ${catId}',
+            mrkdwn_in: ['text']
+        }
+    ]
+
+    const msgDefaults = {
+        response_type: 'in_channel',
+        username: 'Starbot',
+        icon_emoji: config('ICON_EMOJI'),
+        attachments: constructionAttachment
+    }
+
+    let callback = function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+
+            console.log("===BODY===")
+            console.log(JSON.parse(body))
+
+            let msg = _.defaults({
+                channel: payload.channel_name,
+                attachments: attachments
+            }, msgDefaults)
+
+            res.set('content-type', 'application/json')
+            res.status(200).json(msg)
+        }
+        return;
+    }
+
+    let options = {
+        url: `https://geobigdata.io/orders/v2/order`,
+        method: `POST`,
+        headers: {
+            'Authorization':`Bearer ${token}`,
+            'Content-Type': 'application/json',
+        },
+        body: `[${catId}]`,
+        json: true
+    }
+
+    request(options,callback)
+
+    return;
+
+}
+
+module.exports = { pattern: /order/ig, handler: handler }
